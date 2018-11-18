6ad7bebda07957db5f549a62876001d65b8c4739
Randy Herasme <flatironschool@Flatirons-MacBook-Air.local>
2018-11-18 16:16:50 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Write your solution here!
+const drivers = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendDriver(item){
+  drivers.push(item)
+}
+
+function destructivelyPrependDriver(item){
+  drivers.unshift(item)
+}
+
+function destructivelyRemoveLastDriver(){
+  drivers.pop()
+}
+
+function destructivelyRemoveFirstDriver(){
+  drivers.shift()
+}
+
+function appendDriver(item){
+  let new_drivers = [...drivers, item]
+  return new_drivers
+}
+
+function prependDriver(item){
+  let new_drivers = [item, ...drivers]
+  return new_drivers
+}
+
+function removeLastDriver(){
+  let new_drivers = drivers.slice(0, drivers.length - 1)
+  return new_drivers
+}
+
+function removeFirstDriver(){
+  let new_drivers = drivers.slice(1)
+  return new_drivers
+}
